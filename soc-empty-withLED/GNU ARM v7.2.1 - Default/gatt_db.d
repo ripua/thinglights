6e803ea178328b02b2e50717f5527a5cfcb36c10
George Kellerman <george.kellerman@arrow.com>
2018-08-06 18:49:41 -0500

    should not be checked into version control

--- soc-empty-withLED/GNU ARM v7.2.1 - Default/gatt_db.d
+++
@@ -1,7 +0,0 @@
-gatt_db.o: ../gatt_db.c \
- C:\Users\gkellerman\SimplicityStudio\v4_workspace\soc-empty-withLED\protocol\bluetooth\ble_stack\inc\common/bg_gattdb_def.h \
- C:\Users\gkellerman\SimplicityStudio\v4_workspace\soc-empty-withLED\protocol\bluetooth\ble_stack\inc\common/bg_types.h
-
-C:\Users\gkellerman\SimplicityStudio\v4_workspace\soc-empty-withLED\protocol\bluetooth\ble_stack\inc\common/bg_gattdb_def.h:
-
-C:\Users\gkellerman\SimplicityStudio\v4_workspace\soc-empty-withLED\protocol\bluetooth\ble_stack\inc\common/bg_types.h:
--- soc-empty-withLED/GNU ARM v7.2.1 - Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/gkellerman/SimplicityStudio/v4_workspace/soc-empty-withLED/protocol/bluetooth/lib/EFR32MG12P/GCC/libmbedtls.a C:/Users/gkellerman/SimplicityStudio/v4_workspace/soc-empty-withLED/protocol/bluetooth/lib/EFR32MG12P/GCC/binapploader.o C:/Users/gkellerman/SimplicityStudio/v4_workspace/soc-empty-withLED/protocol/bluetooth/lib/EFR32MG12P/GCC/libbluetooth.a C:/Users/gkellerman/SimplicityStudio/v4_workspace/soc-empty-withLED/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg12_gcc_release.a C:/Users/gkellerman/SimplicityStudio/v4_workspace/soc-empty-withLED/protocol/bluetooth/lib/EFR32MG12P/GCC/libpsstore.a
-
-LIBS := -lm
-
